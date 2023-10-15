056c03dafa1354f91203abe66a26c58d79a85627
Mohamed KADI <handskadi@gmail.com>
2023-10-15 02:36:12 +0000

    Classes and attributes for User City ...etc

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -30,7 +30,16 @@ class FileStorage:
     def classes(self):
         """Dic of classes & thier refs."""
         from models.base_model import BaseModel
-        classes = {"BaseModel": BaseModel}
+        from models.user import User
+        classes = {
+                "BaseModel": BaseModel,
+                "User": User,
+                "State": State,
+                "City": City,
+                "Amenity": Amenity,
+                "Place": Place,
+                "Review": Review
+                }
         return classes
 
     def attributes(self):
@@ -40,6 +49,42 @@ class FileStorage:
                     "id": str,
                     "created_at": datetime.datetime,
                     "updated_at": datetime.datetime
+                },
+                "User": {
+                    "email": str,
+                    "password": str,
+                    "first_name": str,
+                    "last_name": str
+                },
+                "State": {
+                    "name": str
+                },
+                "City": {
+                    "state_id": str,
+                    "name": str
+                }
+                "Amenity": {
+                    "name": str
+                },
+                "Place":
+                {
+                    "city_id": str,
+                    "user_id": str,
+                    "name": str,
+                    "description": str,
+                    "number_rooms": int,
+                    "number_bathrooms": int,
+                    "max_guest": int,
+                    "price_by_night": int,
+                    "latitude": float,
+                    "longitude": float,
+                    "amenity_ids": list
+                },
+                "Review":
+                {
+                    "place_id": str,
+                    "user_id": str,
+                    "text": str
                 }
         }
         return attributes
--- models/user.py
+++ models/user.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-"""Module creates User class"""
+"""Module creates User class."""
 from models.base_model import BaseModel
 
 
 class User(BaseModel):
-    """Class for managing users objects"""
+    """Class for managing users objects."""
 
     email = ""
     password = ""
